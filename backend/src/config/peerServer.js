a215f36f5cce7a2604f8f1ab6d19329d8296bb80
Debasish Das <debasish2000.26@gmail.com>
2025-01-17 23:33:11 +0530

    updated peerjs

--- backend/src/config/peerServer.js
+++ backend/src/config/peerServer.js
@@ -6,6 +6,10 @@ function configurePeerServer(peerServer) {
   peerServer.on("disconnect", (client) => {
     console.log(`Peer disconnected: ${client.getId()}`);
   });
+
+  peerServer.on("error", (err) => {
+    console.error("Peer server error:", err);
+  });
 }
 
 module.exports = { configurePeerServer };
