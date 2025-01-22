e45b841a67aecec8d48ac8fabc7aaa16fa43e9c3
Debasish Das <debasish2000.26@gmail.com>
2025-01-22 21:07:47 +0530

    edited viteconfig

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
 
 export default defineConfig({
+  build: {
+    outDir: "dist", // default output folder
+  },
   plugins: [react()],
   css: {
     postcss: {
